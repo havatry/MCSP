0a5b6ff3abfeba7622aa0bdb838b563a673c3fb0
havatry <zylttr@163.com>
2020-03-17 19:42:25 +0800

    调整Main类位置 增加输出到Excel功能

---
+++ src/com/lt/trail/DesignExcel.java
@@ -0,0 +1,80 @@
+package com.lt.trail;
+
+import org.apache.poi.hssf.usermodel.*;
+import org.apache.poi.ss.util.CellRangeAddress;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 设计输出的Excel格式
+ */
+public class DesignExcel {
+    private HSSFWorkbook workbook;
+    private HSSFSheet sheet;
+
+    public DesignExcel() {
+        // 初始化一个Excel表格
+        workbook = new HSSFWorkbook();
+        sheet = workbook.createSheet("MSCP");
+        // 冻结前两列
+        sheet.createFreezePane(0, 2);
+        init();
+    }
+
+    // 初始化表格样式
+    private void init() {
+        // 第一行
+        sheet.createRow(0).createCell(9).setCellValue("MBiLAD");
+        CellRangeAddress address = new CellRangeAddress(0, 0, 9, 13);
+        sheet.addMergedRegion(address); // 第一行将MBiLAD
+        // 第二行
+        HSSFRow row = sheet.createRow(1);
+        row.createCell(0).setCellValue("节点数");
+        row.createCell(1).setCellValue("总边数");
+        row.createCell(2).setCellValue("平均度");
+        row.createCell(3).setCellValue("最小度");
+        row.createCell(4).setCellValue("最大度");
+        row.createCell(5).setCellValue("最小延时");
+        row.createCell(6).setCellValue("最小丢包");
+        row.createCell(7).setCellValue("延时约束");
+        row.createCell(8).setCellValue("丢包约束");
+        row.createCell(9).setCellValue("c");
+        row.createCell(10).setCellValue("d");
+        row.createCell(11).setCellValue("l");
+        row.createCell(12).setCellValue("调用次数");
+        row.createCell(13).setCellValue("运行时间/ms");
+    }
+
+    // 向excel中写入数据
+    protected void writeData(int r, Object[] data) {
+        HSSFRow row = sheet.createRow(r);
+        HSSFCellStyle style = workbook.createCellStyle();
+        style.setDataFormat(HSSFDataFormat.getBuiltinFormat("0.00"));
+        for (int i = 0; i < data.length; i++) {
+            if (i == 2) {
+                HSSFCell cell = row.createCell(i);
+                cell.setCellStyle(style);
+                cell.setCellValue((Double)data[i]);
+            } else {
+                try {
+                    row.createCell(i).setCellValue((Integer) data[i]);
+                } catch (ClassCastException e) {
+                    System.out.println(data);
+                }
+            }
+        }
+    }
+
+    protected void close() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss");
+        try {
+            workbook.write(new FileOutputStream("resource/excel/result_" + sdf.format(new Date()) + ".xls"));
+            workbook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/com/lt/trail/Main.java
@@ -0,0 +1,163 @@
+package com.lt.trail;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+import com.lt.multiAlg.AbstractMCSPMethods;
+import com.lt.multiAlg.MBiLAD;
+import cspAlgorithms.Common;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.lt.topology.MTopology;
+
+import fileInput.IdFile;
+import randomTopology.Constant;
+
+/**
+ * Test MBiLAD class
+ * 2020年1月6日 下午11:34:39
+ */
+public class Main {
+	private AbstractMCSPMethods abstractMCSPMethods;
+	private Integer start;
+	private Integer end;
+	private Integer delayConstraint;
+	private Integer lossConstraint;
+	private int callTime;
+	private DesignExcel designExcel = new DesignExcel();
+	private static Logger log = LoggerFactory.getLogger(Main.class);
+	private final static boolean SPEC = false;
+
+    /**
+     * 对算例进行测试， 一般需要指定Write_TimeFor的变量来测试特定文件
+     * @return 测试结果
+     */
+	public double[] compute() {
+		// 指定文件
+		double[][] Id = IdFile.GetId(true);
+		double maxIndex = -1;
+		for (double[] d : Id) {
+			if (Math.max(d[0], d[1]) > maxIndex) {
+				maxIndex = Math.max(d[0], d[1]);
+			}
+		}
+		int[] Node = new int[(int) Math.round(maxIndex + 1)];
+		for (int s = 0; s < Node.length; s++) {
+			Node[s] = s;
+		}
+		int[][] IdLink = IdFile.GetIdLink(Id);
+		// 设置
+        abstractMCSPMethods = new MBiLAD();
+		start = start == null ? 2 : start;
+		end = end == null ? 8 : end;
+		double minDelay = abstractMCSPMethods.GetMinDelay(Node, Id, IdLink, start, end);
+		double minLoss = abstractMCSPMethods.getMinLoss(Node, Id, IdLink, start, end);
+		// 最小度和最大度
+        int[] minAndMaxDegree = Common.GetMinAndMaxDegree(Node.length, Id);
+        // 总边数
+        int edgeNum = Id.length / 2;
+        // 平均度
+        double averageDegree = AbstractMCSPMethods.getAverageDegree(Node, Id);
+		delayConstraint = delayConstraint == null ? delayConstraint = (int)(minDelay + Math.random() * 10 + 1) : delayConstraint;
+		lossConstraint = lossConstraint== null ? (int)(minLoss + Math.random() * 10 + 1) : lossConstraint;
+		log.info("初始变量,测试序号 = {}, 起点 = {}, 终点 = {}, 延时约束 = {}, 丢包约束 = {}, 节点个数 = {}",
+				new Object[] {Constant.WriteFile_TimeFor, start, end, delayConstraint, lossConstraint, 20});
+		long startTime = System.currentTimeMillis();
+		abstractMCSPMethods.OptimalPath(Node, Id, IdLink, start, end, delayConstraint, lossConstraint);
+		long executeTime = System.currentTimeMillis() - startTime;
+		double[] result = new double[3];
+		try {
+			if (abstractMCSPMethods instanceof MBiLAD) {
+				MBiLAD mBiLAD = (MBiLAD)abstractMCSPMethods;
+				Integer v = mBiLAD.getValue();
+				if (v != null && v == 0) {
+//					throw new Exception("MBiLAD can not find a path");
+					log.info("MBiLAD can not find a path");
+                    if (!SPEC) {
+                        designExcel.writeData(Constant.WriteFile_TimeFor + 1, new Object[]
+                                {Node.length, edgeNum, averageDegree,
+                                        minAndMaxDegree[0], minAndMaxDegree[1], (int)minDelay, (int)minLoss, delayConstraint,
+                                        lossConstraint, -1, -1, -1,
+                                        callTime, executeTime});
+                    }
+				} else {
+					// 存在解
+					List<Integer> optimal_path = mBiLAD.getP_negative();
+					result[0] = mBiLAD.Ctheta(optimal_path, Id, IdLink);
+					result[1] = mBiLAD.Ptheta(optimal_path, Id, IdLink);
+					result[2] = mBiLAD.Ltheta(optimal_path, Id, IdLink);
+					callTime = abstractMCSPMethods.getCallDijkstraTime();
+					// 写入
+                    if (!SPEC) {
+                        designExcel.writeData(Constant.WriteFile_TimeFor + 1, new Object[]
+                                {Node.length, edgeNum, averageDegree,
+                                        minAndMaxDegree[0], minAndMaxDegree[1], (int)minDelay, (int)minLoss, delayConstraint,
+                                        lossConstraint, result[0], result[1], result[2],
+                                        callTime, executeTime});
+                    }
+					return result;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+    /**
+     * 构造算例
+     * @param nodeNum 算例的节点数
+     * @return 对算例的计算结果
+     */
+	public double[] compute(Integer nodeNum) {
+		Constant.step = (Constant.numNodes = nodeNum);
+		new MTopology().ProduceTopology();
+		return compute();
+	}
+	
+	public int getCallTime() {
+		return callTime;
+	}
+	
+	public void setStart(int start) {
+		this.start = start;
+	}
+	
+	public void setEnd(int end) {
+		this.end = end;
+	}
+	
+	public void setDelayConstraint(Integer delayConstraint) {
+		this.delayConstraint = delayConstraint;
+	}
+	
+	public void setLossConstraint(Integer lossConstraint) {
+		this.lossConstraint = lossConstraint;
+	}
+	
+	public static void main(String[] args) {
+		Main main = new Main();
+		if (SPEC) {
+            Constant.WriteFile_TimeFor = 7;
+            main.compute();
+            System.out.println(main.callTime);
+        } else {
+            for (int i = 0; i < 100; i++) {
+                // 20个节点的
+                double[] result = main.compute(20);
+                if (result != null) {
+                    log.info("运算结果: 代价 = {}, 延时 = {}, 丢包 = {}, 调用次数 = {}", new Object[]{result[0], result[1], result[2], main.callTime});
+                }
+                log.info("");
+                main.start = null;
+                main.end = null;
+                main.delayConstraint = null;
+                main.lossConstraint = null;
+                Constant.WriteFile_TimeFor++;
+                Constant.TimeForTest++;
+            }
+            main.designExcel.close();
+        }
+	}
+}
